1c20d247093358273b09f05ea219b95d68591f61
Jeremy Zongker <jeremy@trilitech.com>
2021-02-21 01:10:44 +0000

    Moved css to /public

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 import { EnvironmentHelper } from "./components";
 import App from "./App";
-import "./index.css";
 
 EnvironmentHelper.init();
 ReactDOM.render(
