0aaa6d6df43c2b2af6ad3afdd38ed74587a1ab7a
pranavmalvawala <pranavmalvawala@gmail.com>
2021-03-11 23:13:14 +0530

    default to a theme color

--- src/helpers/ConfigHelper.ts
+++ src/helpers/ConfigHelper.ts
@@ -5,7 +5,7 @@ export interface LogoInterface { url: string, image: string }
 export interface ButtonInterface { text: string, url: string }
 export interface TabInterface { text: string, url: string, icon: string, type: string, data: string, updated?: boolean }
 export interface ServiceInterface { videoUrl: string, serviceTime: string, duration: string, earlyStart: string, chatBefore: string, chatAfter: string, provider: string, providerKey: string, localCountdownTime?: Date, localStartTime?: Date, localEndTime?: Date, localChatStart?: Date, localChatEnd?: Date, label: string }
-export interface ConfigurationInterface { keyName?: string, churchId?: string, primaryColor?: string, contrastColor?: string, homePageUrl?: string, logoImage?: string, buttons?: ButtonInterface[], tabs?: TabInterface[], services?: ServiceInterface[] }
+export interface ConfigurationInterface { keyName?: string, churchId?: string, primaryColor?: string, primaryContrast?: string, logoImage?: string, buttons?: ButtonInterface[], tabs?: TabInterface[], services?: ServiceInterface[] }
 
 
 export class ConfigHelper {
