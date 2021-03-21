52e2414033d2860839094dcd6e91476284c0227f
Jeremy Zongker <jeremy@trilitech.com>
2021-03-21 05:46:33 +0000

    Removed fileloader

--- src/Unauthenticated.tsx
+++ src/Unauthenticated.tsx
@@ -3,15 +3,11 @@ import { Switch, Route, Redirect } from "react-router-dom";
 import { Login } from "./Login"
 import { Forgot } from "./Forgot"
 import { Home } from "./Home"
-import { FileLoader } from "./appBase/pageComponents"
 
 export const Unauthenticated = () => {
     return (
         <>
             <Switch>
-                <Route path="/files/pages/:id" render={(props) => (
-                    <FileLoader id={props.match.params.id} api="StreamingLiveApi" fileType="text/html" />
-                )} />
                 <Route path="/login/:token" component={Login} ></Route>
                 <Route path="/login" component={Login} ></Route>
                 <Route path="/forgot" component={Forgot} ></Route>
